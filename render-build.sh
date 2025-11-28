d1d525da312e9b29b448c35044bc1b99a9ff8e48
Piyush Tiwari <piyush89101@gmail.com>
2025-11-28 12:15:37 +0530

    Temporarily disable Chrome installation for debugging

--- render-build.sh
+++ render-build.sh
@@ -24,17 +24,17 @@ cd ..
 
 
 # Install Google Chrome
-STORAGE_DIR=/opt/render/project/.render
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Come back to the project's root directory
-else
-  echo "...Using Chrome from cache"
-fi
+# STORAGE_DIR=/opt/render/project/.render
+# if [[ ! -d $STORAGE_DIR/chrome ]]; then
+#   echo "...Downloading Chrome"
+#   mkdir -p $STORAGE_DIR/chrome
+#   cd $STORAGE_DIR/chrome
+#   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+#   rm ./google-chrome-stable_current_amd64.deb
+#   cd $HOME/project/src # Come back to the project's root directory
+# else
+#   echo "...Using Chrome from cache"
+# fi
 
 echo "Build successful!"
