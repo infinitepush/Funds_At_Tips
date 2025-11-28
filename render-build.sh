ee9bd903967fdcb8a56b8bbd89df84e05c68d930
Piyush Tiwari <piyush89101@gmail.com>
2025-11-28 11:29:28 +0530

    added routes

---
+++ render-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install pipenv
+pip install pipenv
+
+# Install Google Chrome
+apt-get update
+apt-get install -y procps
+wget -O /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
+rm /tmp/google-chrome-stable_current_amd64.deb
+
+# Your other build commands...
